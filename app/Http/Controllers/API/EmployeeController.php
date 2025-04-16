8a074a9444d28c1f1ab9ba31acc8b20393f78683
ryanrizki10 <ryanrizky.1012@gmail.com>
2025-04-16 15:42:16 +0700

    Uploaddd

---
+++ app/Http/Controllers/API/EmployeeController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Employees;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+        try {
+            $employees = Employees::orderBy('id', 'desc')->get();
+            return response()->json(['success' => true, 'data' => $employees]);
+        } catch (\Throwable $th) {
+            Log::error('Failed to fetch data employees: ' . $th->getMessage());
+            return response()->json([
+                'success' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+            
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'phone' => 'required',
+
+        ]);
+        
+        if ($validate->fails()) {
+            return response()->json([
+                'success' => false, 
+                'message' => $validate->errors(),
+                
+            ],422);
+        }
+        
+        try {
+            $employees = Employees::create([
+                'user_id' => $request->user_id,
+                'phone' => $request->phone,
+                'address' => $request->address,
+                'is_active' => $request->is_active,
+                'gender' => $request->gender,
+            ]);
+            return response()->json(['success' => true, 'message' => 'Employees added successfully', 'data' => $employees]);
+        } catch (\Throwable $th) {
+            Log::error('Failed insert : ' . $th->getMessage());
+            return response()->json(['success' => false, 'message' => 'Failed Create Employees'], 500);
+        }
+
+    }
+
+    public function show($id){
+        try {
+            $employees = Employees::with('user')->findOrFail($id);
+            return response()->json(['success' => true, 'message' =>'Show Data Success', 'data' => $employees]);
+        } catch (\Throwable $th) {
+            Log::error('Failed insert : ' . $th->getMessage());
+            return response()->json(['success' => false, 'message' => 'Failed Create Employees'], 500);
+        }
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $validate = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'phone' => 'required',
+
+        ]);
+        
+        if ($validate->fails()) {
+            return response()->json([
+                'success' => false, 
+                'message' => $validate->errors(),
+                
+            ],422);
+        }
+        
+        try {
+            $data = [
+                'user_id' => $request->user_id,
+                'phone' => $request->phone,
+                'address' => $request->address,
+                'is_active' => $request->is_active,
+                'gender' => $request->gender,
+            ];
+
+            $employees = Employees::findOrFail($id);
+            $employees->update($data);
+            return response()->json(['success' => true, 'message' => 'Employees added successfully', 'data' => $employees]);
+        } catch (\Throwable $th) {
+            Log::error('Failed insert : ' . $th->getMessage());
+            return response()->json(['success' => false, 'message' => 'Failed Create Employees'], 500);
+        }
+
+    }
+
+
+}
+
+
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\EmployeeController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/', function() {
+    return "API RUNN";
+});
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:api')->group(function() {
+    
+    Route::get('me', [AuthController::class, 'me']);
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+
+    Route::apiResource('employee', EmployeeController::class);
+
+});
+
